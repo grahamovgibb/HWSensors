8a3a32dac2b9b2f49064b4d38272a6d8661e2b05
Kozel Rogati <kozel@imac-kozel.(none)>
2011-08-19 19:10:56 +0300

    ACPISensors and X3100

---
+++ ACPISensors/ACPISensors.h
@@ -0,0 +1,33 @@
+/*
+ *  ACPIMonitor.h
+ *  HWSensors
+ *
+ *  Created by mozo on 12/11/10.
+ *  Copyright 2010 mozodojo. All rights reserved.
+ *
+ */
+
+#include <IOKit/IOService.h>
+#include "IOKit/acpi/IOACPIPlatformDevice.h"
+#include <IOKit/IOTimerEventSource.h>
+
+class ACPIMonitor : public IOService
+{
+    OSDeclareDefaultStructors(ACPIMonitor)    
+private:
+	IOService*				fakeSMC;
+	IOACPIPlatformDevice *	acpiDevice;
+	OSDictionary*			sensors;
+	
+	bool				addSensor(const char* method, const char* key, const char* type, unsigned char size);
+	bool				addTachometer(const char* method, const char* caption);
+	
+public:
+	virtual IOService*	probe(IOService *provider, SInt32 *score);
+    virtual bool		start(IOService *provider);
+	virtual bool		init(OSDictionary *properties=0);
+	virtual void		free(void);
+	virtual void		stop(IOService *provider);
+	
+	virtual IOReturn	callPlatformFunction(const OSSymbol *functionName, bool waitForFunction, void *param1, void *param2, void *param3, void *param4 ); 
+};
